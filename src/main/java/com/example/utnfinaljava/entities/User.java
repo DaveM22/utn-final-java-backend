713cf5c18d23b17156e7efcc01ff57d83fe75590
David Martinez <davidmartinez3601@gmail.com>
2023-05-09 12:29:44 -0300

    bug lazy load

--- src/main/java/com/example/utnfinaljava/entities/User.java
+++ src/main/java/com/example/utnfinaljava/entities/User.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -40,7 +41,7 @@ public class User implements UserDetails {
     public String email;
     @Column(name = "clave")
     public String clave;
-    @OneToMany
+    @OneToMany(fetch = FetchType.EAGER)
     @JoinColumn(name = "id_usuario", referencedColumnName = "id_usuario")
     private List<UserRole> roles = new ArrayList<>();
 
