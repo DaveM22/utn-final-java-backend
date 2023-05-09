d995acffab09c0498f041beb932530b347d9db7a
David Martinez <davidmartinez3601@gmail.com>
2023-05-09 00:11:58 -0300

    fix cors

--- src/main/java/com/example/utnfinaljava/config/CorsConfig.java
+++ src/main/java/com/example/utnfinaljava/config/CorsConfig.java
@@ -14,12 +14,13 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry){
                 registry.addMapping("/api/auth/**")
-                .allowedOrigins("http://localhost:4200", "https://davem22.github.io")
+                .allowedOrigins("http://localhost:4200", "tps://davem22githubio-production.up.railway.app")
                 .allowedMethods("*")
                 .exposedHeaders("*");
 
                 registry.addMapping("/api/**")
-                .allowedOrigins("https://davem22.github.io","http://localhost:4200")
+                .exposedHeaders("*")
+                .allowedOrigins("htttps://davem22githubio-production.up.railway.app","http://localhost:4200")
                 .allowedMethods("*");
             }
         };
